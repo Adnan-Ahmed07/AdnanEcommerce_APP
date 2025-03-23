af178c0edee4f590fc495500aa44e736508bd148
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-03-23 15:05:41 -0600

    Working on home page menuItem still lot of work to do

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,20 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import React, { useEffect } from 'react'
 
-import { useAppDispatch, useAppSelector } from '@store/reduxHook';
-import { getHomeContent } from './api/actions';
+
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { screenHeight } from '@utils/Constants';
+import MenuHeader from './molecules/MenuHeader';
 
 const Home = () => {
-  const dispatch = useAppDispatch();
-  const { data, loading, error } = useAppSelector((state) => state.home);
-useEffect(() => {
-  dispatch(getHomeContent(1));
-}, [])
+ const insets = useSafeAreaInsets();
+
+ const scrollYGlobal=useSharedValue(0);
+
+ const moveUpStyle=useAnimatedStyle(()=>{
+  const translateY=interpolate(scrollYGlobal.value,[0,100],[0,-100],
+    Extrapolate.CLAMP
+  )
+  return {
+    transform:[{translateY:translateY}]
+  }
+ })
   return (
-    <View>
-      <Text>{ JSON.stringify(data)}</Text>
+    <View style={style.container}> 
+     <View style={{height:Platform.OS==='android'? insets.top:0}}/>
+     <Animated.View style={[moveUpStyle]}>
+     <View>
+      <MenuHeader scrollY={scrollYGlobal}/>
+     </View>
+      </Animated.View>
+
+      <Animated.View style={[moveUpStyle,{height:screenHeight}]}>
+      
+      </Animated.View>
     </View>
   )
 }
-
+const style=StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"#fff"
+  }
+})
 export default Home;
