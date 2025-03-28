60686c8d71e61cb6c8942664d2d6cb46aa4abc8d
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-03-28 07:11:47 -0600

    Working on Slide two Beautiful slide was added.

--- src/modules/home/organisms/AdCarousal.tsx
+++ src/modules/home/organisms/AdCarousal.tsx
@@ -1,12 +1,71 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Pressable,Image } from 'react-native'
+import React, { FC, useState } from 'react'
+import { screenWidth } from '@utils/Constants';
+import Carousel from 'react-native-reanimated-carousel';
+import FlimSlip from '../molecules/FlimSlip';
 
-const AdCarousal = () => {
+
+const AdCarousal:FC<{data:any}>  = ({data}) => {
+ const [active,setActive]=useState(0);
+ const baseOptions={ 
+  vertical:false,
+  width:screenWidth,
+  height:screenWidth*0.8
+ }
+
   return (
     <View>
-      <Text>AdCarousal</Text>
+      <FlimSlip/>
+      <Carousel
+      {...baseOptions}
+      pagingEnabled
+      snapEnabled
+      autoPlay
+      onSnapToItem={(index:any)=>setActive(index)}
+      data={data.data}
+      renderItem={({item}:any)=>(
+       <Pressable style={styles.imageContainer}>
+         <Image source={item?.image_uri}
+          style={styles.imageContainer}
+         />
+       </Pressable>
+      )}
+      />
+      { 
+        active!=null && 
+        <View style={styles.dots}>
+         { 
+          data?.data?.map((item:any,index:any)=>{ 
+            return(
+              <Text>
+
+              </Text>
+            )
+          })
+         }
+          </View>
+      }
     </View>
   )
 }
-
+const styles=StyleSheet.create({ 
+dots:{ 
+  flexDirection:'row',
+  width:100,
+  alignSelf:'center',
+  marginTop:10,
+  justifyContent:'center',
+ 
+  
+},
+imageContainer:{
+  width:'100%',
+  height:'100%',
+},
+img:{ 
+  width:'100%',
+  height:'100%',
+  resizeMode:'cover',
+}
+})
 export default AdCarousal
