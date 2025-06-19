f784959a8d9245fbf035eb57cd1fd46e1a37ed61
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-06-19 11:57:54 -0600

    working on add Cart

--- src/modules/cart/atoms/OrderItem.tsx
+++ src/modules/cart/atoms/OrderItem.tsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
+import React, { FC } from 'react'
+import UniversalAdd from '@modules/products/atoms/UniversalAdd'
 
-const OrderItem = () => {
+const OrderItem:FC<{item:any}> = ({item}) => {
   return (
-    <View>
-      <Text>OrderItem</Text>
+    <View style={styles.flexRow}>
+    <View style={styles.imageContainer}>
+      <Image source={{uri: item?.image_uri}} style={styles.img} />
+      <UniversalAdd item={item} />
+    </View>
     </View>
   )
 }
-
+const styles=StyleSheet.create({ 
+  flexRow:{ 
+    justifyContent:'space-between',
+    flexDirection:'row',
+    alignItems:"flex-start",
+    marginBottom:15,
+    borderBottomWidth:5,
+    paddingVertical:10,
+    borderColor:'#F0F2F5',
+    padding:10
+  },
+  imageContainer:{ 
+    width:"25%",
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  img:{ 
+    resizeMode:'contain',
+    borderWidth:1,
+    height:90,
+    borderColor:"#ccc",
+    width:"100%",
+    marginBottom:10
+  }
+})
 export default OrderItem
